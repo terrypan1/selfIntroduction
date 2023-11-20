610b6d3fcf492477d1a286f93f2c6f0b0db8d79a
terrypan1 <terrypan1@gmail.com>
2023-11-20 10:40:56 +0800

    更新 contact me

--- components/contact.vue
+++ components/contact.vue
@@ -2,12 +2,12 @@
     <section id="contact" class="mt-4 text-bg-dark">
         <div class="container">
             <div class="row">
-                <div class="col-md-4">
+                <div class="col-md-4 d-flex flex-column jutify-content-center contact-1">
                     <h5>Contact me</h5>
                     <p>期待有機會加入貴公司的專業團隊，</p>
                     <p style="margin-top: -15px;">與您共同打造優秀的產品和服務。</p>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-4 contact-1 d-flex flex-column jutify-content-center">
                     <h3>Quick Links</h3>
                     <ul class="list-unstyled">
                         <li style="color: rgb(248, 181, 71); !important"><a href="#hero"
@@ -17,14 +17,30 @@
                         <li><a href="#portfolio" style="color: rgb(248, 181, 71);">Portfolio</a></li>
                     </ul>
                 </div>
-                <div class="col-md-4">
-                    <h5>Connect</h5>
-                    <a href="https://github.com/terrypan1" style="font-size: 40px !important;margin-right: 10px;"><i
-                            class="bi bi-github text-white"></i></a>
-                    <a href="https://github.com/terrypan1" style="font-size: 40px !important;"><i
-                            class="bi bi-facebook text-white"></i></a>
+                <div class="col-md-4 contact-2">
+                    <div class="contact-3">
+                        <h5>Connect</h5>
+                        <a href="https://github.com/terrypan1" style="font-size: 40px !important;margin-right: 10px;"><i
+                                class="bi bi-github text-white"></i></a>
+                        <a href="https://github.com/terrypan1" style="font-size: 40px !important;"><i
+                                class="bi bi-facebook text-white"></i></a>
+                    </div>
                 </div>
             </div>
         </div>
     </section>
 </template>
+<style lang="scss" scoped>
+@media (max-width:1000px) {
+    #contact .contact-1 {
+        display: flex;
+        // justify-content: center;
+        align-items: center;
+    }
+
+    #contact .contact-2 {
+        display: flex;
+        justify-content: center;
+    }
+}
+</style>
